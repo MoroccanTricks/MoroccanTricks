0a3f11275eeb475168e5ae8a0f5c04fc61207af4
MoroccanTricks <paralm28@gmail.com>
2025-05-15 00:40:22 +0100

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,12 @@
-// Language switcher script
+function setLanguage(lang) {
+    fetch(`${lang}.json`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll('[data-lang]').forEach(element => {
+                const key = element.getAttribute('data-lang');
+                if (data[key]) {
+                    element.textContent = data[key];
+                }
+            });
+        });
+}
